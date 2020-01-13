50597e72f2e40f1115dee90ecee0166b5f2a4e24
Julia Luz dos Santos <julia.15081@gmail.com>
2020-01-13 20:33:39 -0300

    feat: add floodfill, mouse events and neighbor count

--- p5/cell.js
+++ p5/cell.js
@@ -1,14 +1,91 @@
 class Cell {
-    constructor(x, y, w) {
-        this.mine = true;
-        this.revealed = false;
-        this.x = x;
-        this.y = y;
-        this.w = w;
+    constructor(i, j, width) {
+        this.i = i;
+        this.j = j;
+        
+        this.x = i * width; // horizontal
+        this.y = j * width; // vertical
+        this.width = width;
+        
+        this.isRevealed = false;
+        this.neighborCount = 0;
+
+        this.isMine = false;
     }
 
 }
 
 Cell.prototype.show = function () {
-    rect(this.x, this.y, this.w, this.w);
+    stroke(0);
+    noFill();
+    rect(this.x, this.y, this.width, this.width);
+    
+    if (this.isRevealed) {
+        fill(127);
+        if (this.isMine) {
+            ellipse(this.x + this.width * 0.5, this.y + this.width * 0.5, this.width * 0.5)
+        } else {
+            rect(this.x, this.y, this.width, this.width);
+            textAlign(CENTER);
+            if (this.neighborCount > 0) {
+                fill(0);
+                text(this.neighborCount, this.x + this.width * 0.5, this.y + this.width * 0.8);
+            }
+        }
+    }
 }
+
+Cell.prototype.countNeighbors = function () {
+    if (this.isMine) {
+        this.neighborCount = -1;
+        return -1;
+    } 
+
+    var neighborCount = 0;
+
+    for (var xOffset = -1; xOffset <= 1; xOffset++) {
+        for (var yOffset = -1; yOffset <= 1; yOffset++) {
+            var i = this.i + xOffset;
+            var j = this.j + yOffset;
+
+            if (i > -1 && i < cols && j > -1 && j < rows) {   
+                var neighbor = grid[i][j];
+                
+                if (neighbor.isMine) neighborCount++;
+            }
+        }
+    }
+    this.neighborCount = neighborCount;
+}
+
+Cell.prototype.contains = function (x, y) {
+    var xIsValid = x > this.x && x < this.x + this.width;
+    var yIsValid = y > this.y && y < this.y + this.width;
+    return xIsValid && yIsValid;
+}
+
+Cell.prototype.reveal = function () {
+
+    this.isRevealed = true;
+
+    if (this.neighborCount === 0) {
+        this.floodFill();
+    }
+}
+
+Cell.prototype.floodFill = function() {
+    
+    for (var xOffset = -1; xOffset <= 1; xOffset++) {
+        for (var yOffset = -1; yOffset <= 1; yOffset++) {
+            var i = this.i + xOffset;
+            var j = this.j + yOffset;
+
+            if (i > -1 && i < cols && j > -1 && j < rows) {  
+
+                var neighbor = grid[i][j];
+                
+                if (!neighbor.isMine && !neighbor.isRevealed) neighbor.reveal();
+            }
+        }
+    }
+} 
--- p5/sketch.js
+++ p5/sketch.js
@@ -3,9 +3,10 @@ let grid;
 let rows;
 let cols;
 const w = 20;
+const totalMines = 10;
 
 function setup() {
-    createCanvas(200, 200);
+    createCanvas(201, 201);
 
     rows = floor(width / w);
     cols = floor(height / w);
@@ -14,16 +15,61 @@ function setup() {
 
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
-            grid[i][j] = new Cell(i * w, j * w, w);
+            grid[i][j] = new Cell(i, j, w);
+        }
+    }
+
+    let options = [];
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            options.push([i, j]);   
+        }
+    }
+
+    for (let n = 0; n < totalMines; n++) {
+        const index = floor(random(options.length));
+        let choice = options[index];
+        let i = choice[0];
+        let j = choice[1];
+        options.splice(index, 1);
+        grid[i][j].isMine = true;
+    }
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            grid[i][j].countNeighbors();
         }
     }
 }
 
 function draw() {
-    background(0);
+    background(255);
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
             grid[i][j].show();
+        }
+    }
+}
+
+function gameOver() {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            grid[i][j].isRevealed = true;
+        }
+    }
+}
+
+function mousePressed() {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (grid[i][j].contains(mouseX, mouseY)) {
+
+                if (grid[i][j].isMine) {
+                    return this.gameOver();
+                }
+
+                grid[i][j].reveal();
+            }
         }
     }
 }
@@ -35,11 +81,3 @@ function createMatrix(rows, cols) {
     }
     return array;
 }
-
-[
-    ['', '', 1, 1],
-    [1, 1, 2, 'b'],
-    [1, 'b', 2, 1],
-    [1, 1, 1, ''],
-    ['', '', '', '']
-]
